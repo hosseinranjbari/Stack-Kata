d425f6a4c34e875aa7d4796888284346e2851534
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2019-04-28 19:23:30 +0200

    init maven project with tests

---
+++ stack/src/test/java/StackShould.java
@@ -0,0 +1,11 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class StackShould {
+
+    @Test
+    public void works() {
+        Assertions.assertThat(true).isTrue();
+    }
+
+}
